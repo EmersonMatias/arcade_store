3e1f09fff2f4546356551244327378eaae2bcc31
Emerson <emersonsmatthias@gmail.com>
2023-10-30 21:23:49 -0300

    feat: BannerSection style finalized

--- src/pages/Home/Home_Page.tsx
+++ src/pages/Home/Home_Page.tsx
@@ -1,8 +1,9 @@
 
+import WhiteButton from "../../components/WhiteButton/WhiteButton";
 import BestSellersSection from "./BestSellersSection/BestSellersSection";
 import CategorySection from "./CategorySection/CategorySection";
 import HeroSection from "./HeroSection/HeroSection";
-import Container from "./Home_Styles";
+import Container, { Banner } from "./Home_Styles";
 import PromotionSection from "./PromotionSection/PromotionSection";
 
 export default function Home_Page() {
@@ -10,11 +11,21 @@ export default function Home_Page() {
     <Container>
       <HeroSection />
 
-      <BestSellersSection/>
+      <BestSellersSection />
 
       <CategorySection />
 
-      <PromotionSection/>
+      <PromotionSection />
+
+      <Banner>
+        <img className="bannerImg" src="https://olhardigital.com.br/wp-content/uploads/2023/05/Games-Brasil-e1686075484409.jpg" />
+        <div className="gradient"></div>
+        <div className="bannerContent">
+          <h3 className="bannerContentTitle">GASTE E GANHE</h3>
+          <p className="bannerContentText">Economize 20% ao gastar mais de R$ 500,00</p>
+          <WhiteButton name="Comprar agora" />
+        </div>
+      </Banner>
     </Container>
   )
 }
--- src/pages/Home/Home_Styles.tsx
+++ src/pages/Home/Home_Styles.tsx
@@ -13,6 +13,48 @@ const Container = styled.div`
     }
 `
 
+export const Banner = styled.div`
+    width: 100%;
+    height: 45rem;
+    background-color: azure;
+    color: #FFFFFF;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    .bannerImg{
+        width: 100%;
+        top: -25rem;
+        position: absolute;
+    }
+
+    .gradient{
+        width: 100%;
+        height: 100%;
+        background-color: #000000a6;
+        position: absolute;
+    }
+
+    .bannerContent{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        z-index: 2;
+        .bannerContentTitle{
+            font-size: 8rem;
+            font-weight: bold;
+        }
+
+        .bannerContentText{
+            font-size: 4rem;
+            margin-bottom: 4rem;
+        }
+    }
+`
+
 
 
 
