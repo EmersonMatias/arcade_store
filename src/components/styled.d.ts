f064754b2f560245dd3bdf7ea4bf96821ef2d0bc
Emerson <emersonsmatthias@gmail.com>
2023-11-20 17:43:47 -0300

    feat: generic heading1-heading6 and paragraph elements created

--- src/components/styled.d.ts
+++ src/components/styled.d.ts
@@ -6,27 +6,36 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     fonts: {
-        h1: string,
-        h2: string,
-        h3: string,
-        h4: string,
-        h5: string,
-        h6: string,
-        p: string,
+      h1: string,
+      h2: string,
+      h3: string,
+      h4: string,
+      h5: string,
+      h6: string,
+      p: string,
+    },
+    lineHeight: {
+      h1: string,
+      h2: string,
+      h3: string,
+      h4: string,
+      h5: string,
+      h6: string,
+      p: string,
     }
 
     size: {
-        px16: string,
-        px24: string,
-        px32: string,
-        px48: string,
-        px80: string,
-        px40: string,
-        px160: string,
-        px200: string,
-        px680: string,
-        px700: string
-        px820: string,
+      px16: string,
+      px24: string,
+      px32: string,
+      px48: string,
+      px80: string,
+      px40: string,
+      px160: string,
+      px200: string,
+      px680: string,
+      px700: string
+      px820: string,
 
     }
 
--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
 
-export const  theme: DefaultTheme = {
+export const theme: DefaultTheme = {
     fonts: {
         h1: "5.054rem",
         h2: "3.858rem",
@@ -9,19 +9,27 @@ export const  theme: DefaultTheme = {
         h4: "2.248rem",
         h5: "1.716rem",
         h6: "1.31rem",
-        p: "2rem",
+        p: "1rem",
     },
-
+    lineHeight: {
+        h1: "5.938rem",
+        h2: "4.5rem",
+        h3: "3.438rem",
+        h4: "2.625rem",
+        h5: "2rem",
+        h6: "1.563rem",
+        p: "1.188rem",
+    },
     size: {
         px16: "1rem",
         px24: "1.5rem",
         px32: "2rem",
         px48: "3rem",
-        px80: "5rem", 
-        px40: "2.5rem", 
-        px160: "10rem", 
-        px200: "12.5rem", 
-        px680: "42.5rem", 
+        px80: "5rem",
+        px40: "2.5rem",
+        px160: "10rem",
+        px200: "12.5rem",
+        px680: "42.5rem",
         px820: "51.25rem",
         px700: "43.75rem"
     }
